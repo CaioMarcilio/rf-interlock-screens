c779a7ed42e53998ca5f630f19ecefcf3e39ce03
Caio Marcilio <caio.santos@cnpem.br>
2023-02-15 17:54:12 -0300

    Updated setup.sh and README.md

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "---- Installing Anaconda ----"
-sudo wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
-sudo ./Anaconda3-2022.10-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+chmod +x Anaconda3-2022.10-Linux-x86_64.sh
+./Anaconda3-2022.10-Linux-x86_64.sh
 
 echo "---- Preparing Environment ----"
+conda activate
 conda create -n rf-interlock-screens python=3.8 "pyqt >=5,<5.15" pip numpy scipy six psutil pyqtgraph pydm -c conda-forge
 conda activate rf-interlock-screens
 
