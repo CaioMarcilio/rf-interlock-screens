de8fa8987f6408bf676f6f36c51131285473174f
Caio Marcilio <caio.santos@cnpem.br>
2023-02-15 18:13:12 -0300

    Updated setup.sh to activate Anaconda from source

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ echo "---- Installing Anaconda ----"
 wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
 chmod +x Anaconda3-2022.10-Linux-x86_64.sh
 ./Anaconda3-2022.10-Linux-x86_64.sh
+export CONDA_ACTIVATE=/root/anaconda3/bin/activate
+source $CONDA_ACTIVATE 
+conda init
 
 echo "---- Preparing Environment ----"
-conda activate
 conda create -n rf-interlock-screens python=3.8 "pyqt >=5,<5.15" pip numpy scipy six psutil pyqtgraph pydm -c conda-forge
 conda activate rf-interlock-screens
 
